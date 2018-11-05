9eda7b006990cb2312963f2bf5b6d73eff0c4ff4
Riken Maharjan <rikenm@bu.edu>
2018-11-04 19:21:54 -0500

    initial commit

---
+++ Proximity BART/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proximity BART
+//
+//  Created by Riken Maharjan on 11/4/18.
+//  Copyright © 2018 Riken Maharjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
